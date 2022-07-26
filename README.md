c115995d2b19961d673aac48cccc30859cf96aee
moon <xmun74@naver.com>
2022-07-26 15:15:54 +0900

    README, css minimizer plugin

---
+++ README.md
@@ -0,0 +1,3 @@
+# Webpack without CRA(create-react-app)
+
+- [github pages](https://xmun74.github.io/webpack-react/)
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -42,4 +42,7 @@ module.exports = {
     new MiniCssExtractPlugin(),
     new BundleAnalyzerPlugin(),
   ],
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
 };
